9e00c9e17fd828b7d2ddbdd129e8e92ba2a64191
jason <jason.kleinert@twdb.texas.gov>
2021-01-22 13:52:00 -0600

    added map components for catalog and collection routes

--- src/components/routes/CatalogRoute.jsx
+++ src/components/routes/CatalogRoute.jsx
@@ -1,25 +1,37 @@
-//package imports
+// package imports
 import { Col, Row } from "antd"
 
-//local imports
+// local imports
 import { CollectionList } from '../CollectionList';
+import { CatalogMap } from '../CatalogMap';
 import useQuery from "../../utilities/custom-hooks/useQuery";
 import { Link } from "react-router-dom";
 
 export default function CatalogRoute() {
 
-    const map = useQuery().get("map")
+  const map = useQuery().get("map")
   return (
     <div style={{ height: "calc(100% - 30px)" }}>
       <h2>Catalog Route | { map || "map param not set"}</h2>
       <Link to="/?map=true">Show Map</Link> | <Link to="/?map=false">Hide Map</Link>
       <Row style={{ height: "calc(100% - 30px)" }}>
-        <Col span={ map === "true" ? 12 : 24} style={{ border: "solid 1px #666", height: "100%", overflowY: 'scroll', overflowX: 'hidden'  }}>
+        <Col
+          span={ map === "true" ? 8 : 24}
+          style={{
+            border: "solid 1px #666",
+            height: "100%",
+            overflowY: 'scroll',
+            overflowX: 'hidden'
+          }}
+        >
           Collections
-          {/* <CollectionList mapview={map}/> */}
+          <CollectionList mapview={map}/>
         </Col>
-        <Col span={map === "true" ? 12 : 0} style={{ border: "solid 1px #666" }}>
-          Map
+        <Col
+          span={map === "true" ? 16  : 0}
+          style={{ border: "solid 1px #666" }}
+        >
+          <CatalogMap/>
         </Col>
       </Row>
     </div>
