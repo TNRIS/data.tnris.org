b6aa9312ae9c9cfd25c2dc2d259e7fe16da39cdf
Chris Repka <chris.repka@twdb.texas.gov>
2021-01-26 16:07:57 -0600

    back button

--- src/layouts/root/components/HeaderContainer.jsx
+++ src/layouts/root/components/HeaderContainer.jsx
@@ -1,15 +1,16 @@
 import { PageHeader } from "antd";
 import { Header } from "antd/lib/layout/layout";
-import { useHistory } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 
 export function HeaderContainer(props) {
+  const location = useLocation();
   const history = useHistory();
 
   return (
     <div {...props}>
       <Header></Header>
       <PageHeader
-        onBack={ () => console.log(history.goBack)}
+        onBack={ location.pathname !== "/" ? () => history.goBack() : null}
         title={"Dynamic Title Goes Here on every page"}
       />
     </div>
