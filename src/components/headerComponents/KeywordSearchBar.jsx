c84e7eb2b00b499d1a15e5e442aac9dcea320549
Chris Repka <chris.repka@twdb.texas.gov>
2021-03-17 08:19:35 -0500

    Fixed keyword search bug, removed unnecessary geometry files
    * PROBLEM: keyword search was not clearing url param when clear button pressed. Ant Design documentation states that when pressing clear, the onSearch function should be called. This was not happening.
    
    * SOLUTION: conditionally fire history change with changeParams function onChange if the new value is blank. This works when manually backspacing as well as with the clear button, so may ultimately be a better solution despite the bug in Ant Design described above.
    
    * Also removed county and quad centroid files as they take up a lot of space

--- src/components/headerComponents/KeywordSearchBar.jsx
+++ src/components/headerComponents/KeywordSearchBar.jsx
@@ -1,32 +1,55 @@
 import { Input } from "antd";
 import { useState } from "react";
 import { useHistory, useLocation } from "react-router-dom";
-import useQueryParam from "../../utilities/custom-hooks/useQueryParam";
 import { changeParams } from "../../utilities/changeParamsUtil";
+import useQueryParam from "../../utilities/custom-hooks/useQueryParam";
 
 export function KeywordSearchBar() {
   const history = useHistory();
   const search = useLocation().search;
-  const param = useQueryParam().get("s")
+  const param = useQueryParam().get("s");
   const [searchbar, setSearchbar] = useState(param);
+
   return (
     <>
       <Input.Search
+        allowClear
         style={{ width: "50%", minWidth: "300px" }}
         placeholder="Search collections by keyword"
         value={searchbar}
         onChange={(e) => {
-          console.log(e.target.value);
           setSearchbar(e.target.value);
+          if(!e.target.value) {
+            history.push({
+              pathname: "/",
+              search: changeParams(
+                [
+                  {
+                    key: "s",
+                    value: e.target.value,
+                    ACTION: e.target.value ? "set" : "delete",
+                  },
+                ],
+                search
+              ),
+            })
+          }
         }}
-        onSearch={(e) => history.push({
-          pathname: "/",
-          search: changeParams([{
-            key: "s",
-            value: searchbar,
-            ACTION: searchbar ? "set" : "delete"
-          }], search)
-        })}
+        onSearch={(e) =>
+          history.push({
+            pathname: "/",
+            search: changeParams(
+              [
+                {
+                  key: "s",
+                  value: searchbar,
+                  ACTION: searchbar ? "set" : "delete",
+                },
+              ],
+              search
+            ),
+          })
+        }
       />
     </>
   );
