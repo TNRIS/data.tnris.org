599df9ca8d52838c5705cf8a823846bf5f029abf
Chris Repka <chris.repka@twdb.texas.gov>
2021-04-08 09:26:32 -0500

    Temporary fix for collection with insufficient 'the_geom' field

--- src/utilities/mapHelpers/zoomHelpers.js
+++ src/utilities/mapHelpers/zoomHelpers.js
@@ -3,6 +3,11 @@ import { multiPolygon } from "@turf/helpers";
 
 export const zoomToFeatures = (map, features, padding = 100) => {
   if (map) {
-    map.fitBounds(bbox(multiPolygon(features.coordinates)), { padding: padding });
+    console.log(bbox(multiPolygon(features)));
+    if (features.coordinates.length > 4) {
+      map.fitBounds(bbox(multiPolygon(features.coordinates)), {
+        padding: padding,
+      });
+    }
   }
 };
